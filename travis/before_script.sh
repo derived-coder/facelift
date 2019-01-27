d8bdc3e180d43c111d796ebec15ac8395db2b1df
Derived Coder <acki.dev@gmx.de>
2019-01-27 15:30:00 +0100

    test 32343

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -6,11 +6,4 @@ if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
 fi
 
 
-# OS X 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    #QT_VERSION_SHORT=5.12.0
-    #QT_VERSION=5.12.0
-    #QT_PATH=/Users/$USER/Qt$QT_VERSION
-    #PATH=$QT_PATH/$QT_VERSION_SHORT/clang_64/bin:$PATH
-    #export Qt5_DIR=$QT_PATH/$QT_VERSION_SHORT/clang_64/lib/cmake/Qt5
-fi
+
